535d4201b1dcc1bad7aa2802a0add693839e7a44
AvirukBasak <avirukbasak@gmail.com>
2022-12-12 14:42:11 +0530

    updated test.c

--- tests/test.c
+++ tests/test.c
@@ -6,12 +6,22 @@ void conn_handler(ServerReq* req, ServerRes* res) {
     res->writeStr(res, "HTTP/1.1 OK\r\n\r\n");
     res->writeStr(res,
         "<html>\n"
+        "<head>\n"
+        "    <meta charset=\"utf-8\">\n"
+        "    <meta http-equiv=\"X-UA-Compatible\" content=\"IE=edge\">\n"
+        "    <meta name=\"keywords\" content=\"\">\n"
+        "    <meta name=\"description\" content=\"\">\n"
+        "    <meta name=\"viewport\" content=\"width=device-width, height=device-height, user-scalable=no\">\n"
+        "    <title>Demo</title>\n"
+        "</head>\n"
         "<body>\n"
-        "    <pre>recieved data: "
+        "    <pre>\n"
+        "Recieved data:\n"
     );
     res->writeBytes(res, req->data, req->size);
     res->writeStr(res,
-        "...truncated</pre>\n"
+        "...truncated\n"
+        "    </pre>\n"
         "</body>\n"
         "</html>\n"
         "\r\n\r\n"
