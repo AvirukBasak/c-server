6c8092b34ebc6120c53fdb370a385f82d24cbd47
AvirukBasak <avirukbasak@gmail.com>
2022-12-12 17:10:59 +0530

    na

--- src/server.c
+++ src/server.c
@@ -62,39 +62,39 @@ void Server_listen(Server* sv, void (*callback)(ipaddr_t, port_t))
 {
     if (!sv) __server_print_err("null pointer", E_NULLPTR);
     sockfd_t hostfd = __server_socket_listen(sv->priv->addr, sv->priv->port);
-    const char* time = NULL;
+    const char* datetime = NULL;
     printf("[%s] - listening on %d.%d.%d.%d:%d\n",
-        time = __server_std_gettime(),
+        datetime = __server_std_gettime(),
         sv->priv->addr[0],
         sv->priv->addr[1],
         sv->priv->addr[2],
         sv->priv->addr[3],
         sv->priv->port
     );
-    free((void*) time);
+    free((void*) datetime);
     if (callback) callback(sv->priv->addr, sv->priv->port);
     while (true) {
         ServerReq* req = __server_socket_accept(hostfd);
         // log request reciept
         if (req->size) {
-            const char* time = NULL;
+            const char* datetime = NULL;
             printf("[%s] - %d.%d.%d.%d \"%.*s...\"\n",
-                time = __server_std_gettime(),
+                datetime = __server_std_gettime(),
                 req->addr[0], req->addr[1], req->addr[2], req->addr[3],
                 __server_std_get_sub_reqdata_end(req->data),
                 req->data
             );
-            free((void*) time);
+            free((void*) datetime);
         }
         // connection closed by client, drop request
         // note the || below, this if condition is not an inverse of the previous condition
         if (!req->clientfd || !req->size) {
-            const char* time = NULL;
+            const char* datetime = NULL;
             printf("[%s] - %d.%d.%d.%d client closed connection\n",
-                time = __server_std_gettime(),
+                datetime = __server_std_gettime(),
                 req->addr[0], req->addr[1], req->addr[2], req->addr[3]
             );
-            free((void*) time);
+            free((void*) datetime);
             free(req);
             continue;
         }
