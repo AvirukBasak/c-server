36a95c0c61f17ec0a924e5111fd03acf76434011
AvirukBasak <avirukbasak@gmail.com>
2022-12-17 10:04:13 +0530

    added io fn for connection logging

--- include/io.h
+++ include/io.h
@@ -1,6 +1,10 @@
 #ifndef __IO_H__
 #define __IO_H__
 
+#include "types.h"
+
 void server_print_err(const char* msg, int err_code);
+void server_print_connlog(ServerReq* req);
+void server_print_connclose(ServerReq* req);
 
 #endif
--- src/io.c
+++ src/io.c
@@ -1,8 +1,10 @@
 #include <errno.h>     // errno
-#include <stdlib.h>    // exit, abort
+#include <stdlib.h>    // exit, abort, free
 #include <stdio.h>     // fprintf, stderr
 
 #include "io.h"
+#include "request.h"
+#include "stdfunc.h"
 
 void server_print_err(const char* msg, int err_code)
 {
@@ -15,4 +17,26 @@ void server_print_err(const char* msg, int err_code)
 #else
     exit(err_code ? err_code : errno);
 #endif
+}
+
+void server_print_connlog(ServerReq* req)
+{
+    // log request reciept
+    const char* datetime = NULL;
+    printf("[%s] - %d.%d.%d.%d accepted\n",
+        datetime = server_std_gettime(),
+        req->addr[0], req->addr[1], req->addr[2], req->addr[3],
+    );
+    free((void*) datetime);
+}
+
+void server_print_connclose(ServerReq* req)
+{
+    // connection closed by client
+    const char* datetime = NULL;
+    printf("[%s] - %d.%d.%d.%d client closed connection\n",
+        datetime = server_std_gettime(),
+        req->addr[0], req->addr[1], req->addr[2], req->addr[3]
+    );
+    free((void*) datetime);
 }
