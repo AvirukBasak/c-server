e2b510ed35528dc505739bf9d2c0d43242d102b2
AvirukBasak <avirukbasak@gmail.com>
2022-12-18 05:36:56 +0530

    updated socket error fn

--- include/socket.h
+++ include/socket.h
@@ -1,6 +1,7 @@
 #ifndef __SOCKET_H__
 #define __SOCKET_H__
 
+#include <stdbool.h>
 #include <sys/socket.h>    // socket, bind, listen, accept, recv, send
 #include <arpa/inet.h>     // struct sockaddr, struct sockaddr_in, htonl, htons
 
@@ -11,9 +12,10 @@
 #define SOCK_BACKLOG (100)
 #define SOCK_RECVLEN (4 * 4096)
 
-int        server_socket_try    (int retval, const char* msg);
-sockfd_t   server_socket_listen (ipaddr_t addr, port_t port);
-ServerReq* server_socket_accept (sockfd_t hostfd);
-void       server_socket_close  (sockfd_t sockfd);
+int        server_socket_die     (int retval, const char* msg);
+bool       server_socket_isvalid (sockfd_t sockfd);
+sockfd_t   server_socket_listen  (ipaddr_t addr, port_t port);
+ServerReq* server_socket_accept  (sockfd_t hostfd);
+void       server_socket_close   (sockfd_t sockfd);
 
 #endif
--- src/socket.c
+++ src/socket.c
@@ -4,6 +4,7 @@
 #include <inttypes.h>   // uint32_t
 #include <unistd.h>     // close
 #include <stdbool.h>    // bool
+#include <errno.h>      // errno
 
 #include "types.h"
 #include "errcodes.h"
@@ -12,7 +13,7 @@
 #include "server.h"
 #include "socket.h"
 
-int server_socket_try(int retval, const char* msg) {
+int server_socket_die(int retval, const char* msg) {
     if (retval <= -1)
         server_print_err(msg, E_ERRNO);
     else if (retval == STDOUT_FILENO || retval == STDERR_FILENO)
@@ -20,8 +21,16 @@ int server_socket_try(int retval, const char* msg) {
     return retval;
 }
 
+bool server_socket_isvalid(sockfd_t sockfd) {
+    if (sockfd <= 0
+     || sockfd == STDIN_FILENO
+     || sockfd == STDOUT_FILENO
+     || sockfd == STDERR_FILENO) return false;
+    return true;
+}
+
 sockfd_t server_socket_listen(ipaddr_t addr, port_t port) {
-    sockfd_t hostfd = server_socket_try(
+    sockfd_t hostfd = server_socket_die(
         socket(AF_INET, SOCK_STREAM , 0),
         "socket creation failed"
     );
@@ -33,11 +42,11 @@ sockfd_t server_socket_listen(ipaddr_t addr, port_t port) {
     hostaddr.sin_family = AF_INET;
     hostaddr.sin_addr.s_addr = *(uint32_t*) addr;
     hostaddr.sin_port = htons(port);
-    server_socket_try(
+    server_socket_die(
         bind(hostfd, (sockaddr_t*) &hostaddr, sizeof(hostaddr)),
         "socket bind failed"
     );
-    server_socket_try(
+    server_socket_die(
         listen(hostfd, SOCK_BACKLOG),
         "socket listen failed"
     );
@@ -46,13 +55,11 @@ sockfd_t server_socket_listen(ipaddr_t addr, port_t port) {
 
 ServerReq* server_socket_accept(sockfd_t hostfd)
 {
-    server_socket_try(hostfd, "host socket fd invalid");
+    if (!server_socket_isvalid(hostfd)) return NULL;
     sockaddrin_t clientaddr;
     socklen_t len = sizeof(clientaddr);
-    sockfd_t clientfd = server_socket_try(
-        accept(hostfd, (sockaddr_t*) &clientaddr, &len),
-        "socket accept failed"
-    );
+    sockfd_t clientfd = accept(hostfd, (sockaddr_t*) &clientaddr, &len);
+    if (!server_socket_isvalid(clientfd)) return NULL;
     ipaddr_t addr;
     uint32_t sin_addr = clientaddr.sin_addr.s_addr;
     addr[0] = ((uint8_t*) &sin_addr)[0];
@@ -64,6 +71,7 @@ ServerReq* server_socket_accept(sockfd_t hostfd)
 
 void server_socket_close(sockfd_t sockfd)
 {
-    server_socket_try(sockfd, "socket fd invalid");
+    if (!server_socket_isvalid(sockfd))
+        return;
     close(sockfd);
 }
